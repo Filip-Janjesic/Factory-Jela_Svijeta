33cc70a6f35a022bc6151e62c15da55b333ed809
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2022-07-05 22:13:30 +0200

    index

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php include_once 'konfiguracija.php'; ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+	<?php include_once 'include/head.php'; ?>
+	
+</head>
+<body>
+
+	<?php include_once 'include/header.php'; ?>
+ 
+	<?php include_once 'include/content.php'; ?>
+
+	<?php include_once 'include/footer.php'; ?>
+
+</body>
+</html>
