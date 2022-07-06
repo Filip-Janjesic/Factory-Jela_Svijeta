c79588775bc3c0cdb21113f8e7e0239dfa19c0c6
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2022-07-06 23:26:41 +0200

    Create era.php

---
+++ era.php
@@ -0,0 +1,32 @@
+<?php include_once 'konfiguracija.php';?>
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include_once 'include/head.php'; ?>
+
+</head>
+<body>
+	<?php include_once 'include/menu-era.php'; ?>		
+
+<!--content-->
+	   <section class="head index-part-1">      
+          <div class="wow">
+          <div class="container">
+            <div class="row">
+               <div class="font-1 helmet text-center">
+                
+                <img src="verzija4.png" alt="ERA">>
+
+               
+               </div>
+            </div>
+          </div>
+        </div>
+    </section>
+
+<!--footer-->
+	<?php include_once 'include/footer.php'; ?>
+	
+
+</body>
+</html>
