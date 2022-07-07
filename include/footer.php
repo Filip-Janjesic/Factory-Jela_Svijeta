70a61aeb81591f6c7d2144a4a8875a13c4c1d214
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2022-07-07 22:31:08 +0200

    Update footer.php

--- include/footer.php
+++ include/footer.php
@@ -24,6 +24,6 @@
 			<div class="clearfix"> </div>
 					
 			</div>
-			<p class="footer-class animated wow bounce" data-wow-duration="1000ms" data-wow-delay="500ms">&copy; 2022. Plava Tvornica - Jela svijeta. Filip Janješić. <?php echo $lang['design'];?> <a href="http://w3layouts.com/" target="_blank">W3layouts</a> </p>
+			<p class="footer-class animated wow bounce" data-wow-duration="1000ms" data-wow-delay="500ms">&copy; 2022. Filip Janješić - Jela svijeta. <?php echo $lang['design'];?> </p>
 		</div>
 	</div>		
