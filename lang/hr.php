7245dc47c12e30143721ea4a87f5ea2e0788fde6
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2022-07-07 13:12:37 +0200

    prijevod

---
+++ lang/hr.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang=array(
+	'hello' => 'Hello',
+	'home' => 'Početna stranica',
+	'menu' => 'Meni',
+	'contact' => 'Kontakt',
+	'hello1' => 'Pozdrav i dobrodošli na Jela Svijeta',
+	'about' => 'O nama',
+	'variation' => 'Postoje mnogobrojne varijacije',
+	'food' => 'Hrana',
+	'follow' => 'Pratite nas',
+	'variation1' => 'Postoje mnoge varijacije prolaza',
+	'design' => 'Dizajn: ',
+	'previous' => 'Prethodna',
+	'next' => 'Sljedeća',
+	'term' => 'Uvjet pretraživanja',
+	'meal' => 'Jelo',
+	'tag' => 'Oznaka',
+	'category' => 'Kategorija',
+	'ingredient' => 'Sastojci'
+
+
+
+
+
+
+
+
+
+
+);
+
+
+
+?>
